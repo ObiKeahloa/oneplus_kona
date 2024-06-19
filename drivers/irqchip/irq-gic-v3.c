Blaster4385/IllusionX_sm8250

treewide: Remove remaining _no_log() usage

drivers/irqchip/irq-gic-v3.c
@@ -103,7 +103,7 @@ static void gic_do_wait_for_rwp(void __iomem *base, u32 bit)
 {
 	u32 count = 1000000;	/* 1s! */
 
-	while (readl_relaxed_no_log(base + GICD_CTLR) & bit) {
+	while (readl_relaxed(base + GICD_CTLR) & bit) {
 		count--;
 		if (!count) {
 			pr_err_ratelimited("RWP timeout, gone fishing\n");
@@ -184,7 +184,7 @@ static int gic_peek_irq(struct irq_data *d, u32 offset)
 	else
 		base = gic_data.dist_base;
 
-	return !!(readl_relaxed_no_log
+	return !!(readl_relaxed
 		(base + offset + (gic_irq(d) / 32) * 4) & mask);
 }
 

ObiKeahloa/oneplus_kona

Signed-off-by: Devang Chaudhary <devangsingh369@gmail.com>

drivers/irqchip/irq-gic-v3.c
@@ -103,7 +103,7 @@ static void gic_do_wait_for_rwp(void __iomem *base)
 {
 	u32 count = 1000000;	/* 1s! */
 
-	while (readl_relaxed_no_log(base + GICD_CTLR) & GICD_CTLR_RWP) {
+	while (readl_relaxed(base + GICD_CTLR) & GICD_CTLR_RWP) {
 		count--;
 		if (!count) {
 			pr_err_ratelimited("RWP timeout, gone fishing\n");
@@ -184,7 +184,7 @@ static int gic_peek_irq(struct irq_data *d, u32 offset)
 	else
 		base = gic_data.dist_base;
 
-	return !!(readl_relaxed_no_log
+	return !!(readl_relaxed
 		(base + offset + (gic_irq(d) / 32) * 4) & mask);
 }
 