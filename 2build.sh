41645868519f63e6cce1cea583a7b3073b5b4b49
ObiKeahloa <63236194+ObiKeahloa@users.noreply.github.com>
2024-09-07 10:12:54 +0100

    changes

--- 2build.sh
+++ 2build.sh
@@ -1,2 +1,3 @@
-make O=out CC=clang AR=llvm-ar LD=ld.lld vendor/kona-perf_defconfig
-make O=out CC=clang AR=llvm-ar LD=ld.lld OBJDUMP=llvm-objdump NM=llvm-nm OBJCOPY=llvm-objcopy STRIP=llvm-strip LLVM=1 LLVM_IAS=1
+make O=out CC=clang AR=llvm-ar LD=ld.lld -j8 vendor/kona-perf_defconfig
+#make O=out CC=clang AR=llvm-ar LD=ld.lld OBJDUMP=llvm-objdump NM=llvm-nm OBJCOPY=llvm-objcopy STRIP=llvm-strip LLVM=1 LLVM_IAS=1 menuconfig
+make O=out CC=clang AR=llvm-ar LD=ld.lld OBJDUMP=llvm-objdump NM=llvm-nm OBJCOPY=llvm-objcopy STRIP=llvm-strip LLVM=1 LLVM_IAS=1 -j8
